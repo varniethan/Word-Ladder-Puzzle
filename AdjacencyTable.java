e9a6081c1fb73616bc6f67dd8bb46beb624cd446
Varniethan Ketheeswaran <varniethan@protonmail.com>
2023-11-16 16:54:49 +0000

    Files added

---
+++ AdjacencyTable.java
@@ -0,0 +1,42 @@
+public class AdjacencyTable {
+
+    private GraphNode[] table;
+
+    public AdjacencyTable(String[] nodes) {
+	// add your constructor code here
+    }
+
+    public GraphNode[] getTable() {
+        return table;
+    }
+
+    public boolean find(String s) {
+	// add your table search code here
+	return false; // delete this line
+    }
+
+    public GraphNode get(String s) {
+	// add your table lookup code here
+	return null; // delete this line
+    }
+
+    public String getPath(String s, String t) {
+	// add your code here
+	return ""; // delete this line
+    }
+
+    public boolean existsPath(String s, String t) {
+	// add your code here
+	return false; // delete this line
+    }
+
+    public int pathLength(String s, String t) {
+	// add your code here
+	return 111; // delete this line
+    }
+
+    public static AdjacencyTable weaver() {
+	// add your code here
+	return null; // delete this line
+    }
+}
---
+++ GraphNode.java
@@ -0,0 +1,17 @@
+ public class GraphNode {
+
+    // A node holds a String label, and an array of its neighbours
+    public String label;
+    public String[] neighbours = new String[6]; // 6 neighbours to start with
+
+    // You may add further fields and methods. 
+     
+    public GraphNode(String label) {
+      this.label = label;
+    }
+
+    public void addNeighbour(String s) {
+       // add your code here
+   }
+
+}
