00c1bc0b462bfb029b1d87b2eed915b079074cbe
varniethan <varniethan@protonmail.com>
2023-11-19 10:17:31 +0000

    TODO: Notfinding path

--- src/AdjacencyTable.java
+++ src/AdjacencyTable.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class AdjacencyTable {
 
     private GraphNode[] table;
@@ -96,8 +98,42 @@ public class AdjacencyTable {
     }
 
     public String getPath(String s, String t) {
-	// add your code here
-	return ""; // delete this line
+        GraphNode start = this.get(s);
+        GraphNode end = this.get(t);
+        if (start != null && end != null) {
+            return "There is no path from " + s + " to " + t + ".";
+        }
+
+        if (start.equals(end)) {
+            return s;
+        }
+
+        Queue<GraphNode> queue = new LinkedList<>();
+        ArrayList<GraphNode> visited = new ArrayList<>();
+        Map<GraphNode, String> pathMap = new HashMap<>();
+
+        queue.add(start);
+        visited.add(start);
+        pathMap.put(start, s);
+
+        while (!queue.isEmpty())
+        {
+            GraphNode currentNode = queue.remove();
+            for (int i = 0; i < currentNode.neighbours.length; i++) {
+                if (!visited.contains(currentNode.neighbours[i])) {
+                    GraphNode neighbour = this.get(currentNode.neighbours[i]);
+                    pathMap.put(neighbour, pathMap.get(currentNode) + " " + neighbour.label);
+                    if (neighbour.equals(end)) {
+                       return pathMap.get(neighbour); //path found
+                    }
+                    else {
+                        queue.add(neighbour);
+                        visited.add(neighbour);
+                    }
+                }
+            }
+        }
+        return "There is no path from " + s + " to " + t + ".";
     }
 
     public boolean existsPath(String s, String t) {
@@ -112,17 +148,108 @@ public class AdjacencyTable {
             existsPath = true;
         }
 
+        Queue<GraphNode> queue = new LinkedList<>();
+        ArrayList<GraphNode> visited = new ArrayList<>();
 
+        queue.add(start);
+        visited.add(start);
 
+        while (!queue.isEmpty()) {
+            GraphNode currentNode = queue.remove();
+            if (currentNode.equals(end)) {
+                existsPath = true;
+                break;
+            }
+            else {
+                for (int i = 0; i < currentNode.neighbours.length; i++) {
+                    if (currentNode.neighbours[i] != null) {
+                        GraphNode neighbour = this.get(currentNode.neighbours[i]);
+                        if (!visited.contains(neighbour)) {
+                            queue.add(neighbour);
+                            visited.add(neighbour);
+                        }
+                    }
+                }
+            }
+        }
+        return existsPath;
     }
 
     public int pathLength(String s, String t) {
-	// add your code here
-	return 111; // delete this line
+        GraphNode start = this.get(s);
+        GraphNode end = this.get(t);
+        int pathLength = 0;
+        if (start != null && end != null) {
+            pathLength = 0;
+        }
+
+        if (start.equals(end)) {
+            pathLength = 1;
+        }
+
+        Queue<GraphNode> queue = new LinkedList<>();
+        ArrayList<GraphNode> visited = new ArrayList<>();
+        Map<GraphNode, Integer> distance = new HashMap<>();
+
+        queue.add(start);
+        visited.add(start);
+        distance.put(start, 1);
+
+
+        while (!queue.isEmpty()) {
+            GraphNode currentNode = queue.remove();
+            int currentDistance = distance.get(currentNode);
+            if (currentNode.equals(end)) {
+                pathLength = currentDistance;
+                break;
+            }
+            else {
+                for (int i = 0; i < currentNode.neighbours.length; i++) {
+                    if (currentNode.neighbours[i] != null) {
+                        GraphNode neighbour = this.get(currentNode.neighbours[i]);
+                        if (!visited.contains(neighbour)) {
+                            queue.add(neighbour);
+                            visited.add(neighbour);
+                            distance.put(neighbour, currentDistance+1);
+                        }
+                    }
+                }
+            }
+        }
+return pathLength;
+    }
+
+    public static List<String> generateNeighbours(String word, String[] words) {
+        List<String> neighbours = new ArrayList<>();
+        for (int i = 0; i < word.length(); i++) {
+            char[] wordArray = word.toCharArray();
+            for (char c = 'a'; c <= 'z'; c++) {
+                wordArray[i] = c;
+                String newWord = new String(wordArray);
+                if (Arrays.asList(words).contains(newWord)) {
+                    neighbours.add(newWord);
+                }
+            }
+        }
+        return neighbours;
+    }
+
+    public static boolean isValidWord(String word, String[] words) {
+        return Arrays.asList(words).contains(word);
     }
 
     public static AdjacencyTable weaver() {
-	// add your code here
-	return null; // delete this line
+        String[] words = WeaverWords.words;
+        AdjacencyTable wordLadder = new AdjacencyTable(words);
+        for (String word : words) {
+            List<String> neighbours = generateNeighbours(word, words);
+            for (String neighbour : neighbours) {
+                if ((isValidWord(neighbour, words)) && (!neighbour.equals(word))) {
+                    GraphNode wordNode = wordLadder.get(word);
+                    wordNode.addNeighbour(neighbour);
+                }
+            }
+        }
+        return wordLadder;
     }
 }
